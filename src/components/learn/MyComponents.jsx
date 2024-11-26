4284c0684264c57ce9c256f4804aee110a6a5e99
dangayaudio <153900798+dangayaudio@users.noreply.github.com>
2024-11-26 15:22:26 +0700

    #30

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,6 +1,18 @@
+import "./style.css";
+
+
 const MyComponents = () => {
     return (
-        <div>test</div>
+        <>
+            <div>test</div>
+            <div className="child"
+                style={{
+                    borderRadius: "30px"
+                }}
+            >
+                test1
+            </div>
+        </>
     );
 }
 export { MyComponents };
